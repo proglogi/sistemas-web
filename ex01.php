3732d840ef03c57def50fb1830dda69e63b35a5f
Nicholly Gonzaga <nichollygonzaga@hotmail.com>
2025-07-29 22:09:35 -0300

    Exercício introdutório

---
+++ ex01.php
@@ -0,0 +1,14 @@
+<?php
+	$uno=4;
+	$dos=2;
+	
+	echo "A soma de $uno com $dos é = " . $uno + $dos . "</br>";
+	echo "A subtração de $uno com $dos é =" . $uno - $dos . "</br>";
+	echo "A divisão de $uno com $dos é = " . $uno / $dos . "</br>";
+	echo "A multiplicação de $uno com $dos é = " . $uno * $dos . "</br>";
+	echo " $uno elevado a $dos é = " . $uno ** $dos . "</br>";
+	echo "O resto entre $uno e $dos é = " . $uno % $dos . "</br>";
+	
+	const nome = "Nicholly";
+	echo "</br> Meu nome é <u><i><strong>" . nome . "</strong></i></u>";
+?>
